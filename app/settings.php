2fb7a71297268ca1823e00e2498e06cc90817650
Hank Sway <hank.sway@gmail.com>
2017-10-16 17:02:04 -0400

    change to webinar-prep branch

---
+++ app/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+	return [
+        'settings' => [
+                // Slim Settings
+                'determineRouteBeforeAppMiddleware' => true,
+                'displayErrorDetails' => true,
+        ],
+    ];
