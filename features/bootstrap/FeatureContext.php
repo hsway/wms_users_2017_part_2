a572ac37727ad907807612a9fae8e06d82cdfd50
Hank Sway <hank.sway@gmail.com>
2017-10-17 11:18:52 -0400

    fixed behat tests by specifing features/test.php when starting php server

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,16 +1,39 @@
 <?php
-
 use Behat\Behat\Hook\Scope\AfterStepScope;
 use Behat\Behat\Context\Context;
 use Behat\MinkExtension\Context\RawMinkContext;
-
+/**
+ * Features context.
+ */
 class FeatureContext extends RawMinkContext implements Context
 {
-
-	public function __construct()
+	
+	/** @AfterStep */
+	public function errors(AfterStepScope $scope)
+	{
+		if (99 === $scope->getTestResult()->getResultCode()) {
+			print static::showError($scope->getEnvironment()->getContext('Behat\MinkExtension\Context\MinkContext')->getSession());
+		}
+	}
+	
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     */
+    public function __construct()
     {
-        
+    	
     }
-
     
+    private static function showError($session) {
+    	if ($session){
+    		$errorMessage = "FAILED: \n";
+    		$errorMessage .= "URL: " . $session->getCurrentUrl(). "\n";
+    		$errorMessage .= "Response: " . $session->getPage()->getContent() . "\n";
+    	} else {
+    		$errorMessage = 'Last Page Information Unknown';
+    	}
+    	return $errorMessage;
+    }
 }
